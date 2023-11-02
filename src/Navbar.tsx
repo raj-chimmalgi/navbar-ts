d232ab392ed66aa436d4b78cc4fc7130c817700b
Raj Chimmalgi <chimmalgi@mgail.com>
2023-11-01 17:15:41 -0700

    navbar for small and big screen

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,21 +1,34 @@
-// import { useState } from "react";
+import { useRef, useState } from "react";
 import { FaBars } from "react-icons/fa";
-import { links } from "./data.jsx";
-import logo from "./logo.svg";
+import { links, social } from "./data.jsx";
+import logo from "./assets/react.svg";
 
 const Navbar = () => {
-  // const [showLinks, setShowLinks] = useState(false);
+  const [showLinks, setShowLinks] = useState(false);
+  const linksRef = useRef(null);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : "0px",
+  };
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className={"logo"} alt="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={toggleLinks}>
             <FaBars />
           </button>
         </div>
-        <div className="links-container">
-          <ul className="links">
+
+        <div className="links-container" style={linkStyles}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => (
               <li key={link.id}>
                 <a href={link.url}>{link.text}</a>
@@ -23,6 +36,13 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
+        <ul className="social-icons">
+          {social.map((item) => (
+            <li key={item.id}>
+              <a href={item.url}>{item.icon}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
